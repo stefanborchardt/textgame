44945cddf9180c9c0d25476afcddfabfa277f78d
Stefan Borchardt <stefan.borchardt@gmail.com>
2018-08-07 21:09:42 +0200

    add primus

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var reqLogger = require('morgan');
 
+var Primus = require('primus');
+
+
 var winston = require('winston');
 const logger = winston.createLogger({
-  transports: [
-    new winston.transports.File({ filename: 'logs/combined.log' })
-  ]
+	transports: [
+		new winston.transports.File({
+			filename: 'logs/combined.log'
+		})
+	]
 });
 
 
@@ -24,18 +29,29 @@ var app = express();
 var https = require('https');
 const fs = require('fs');
 const options = {
-    cert: fs.readFileSync('./sslcert/localhost.crt'),
-    key: fs.readFileSync('./sslcert/localhost.key'),
-    rejectUnauthorized: false
+	cert: fs.readFileSync('./sslcert/textga.me_ssl_certificate.cer'),
+	key: fs.readFileSync('./sslcert/textga.me_private_key.key'),
+	rejectUnauthorized: false
 };
 
 var server = https.createServer(options, app);
 logger.info('server');
+
 /**
  * Create Websocket server.
  */
-var webSocket = require('ws');
-var wsServer = new webSocket.Server({server});
+// var webSocket = require('ws');
+// var wsServer = new webSocket.Server({
+// 	server
+// });
+
+primus = new Primus(server, {
+	transformer: 'websockets',
+	pathname: '/tg',
+	parser: 'json'
+});
+
+//primus.save("public/javascripts/primus.js");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,21 +60,23 @@ app.set('port', 3000);
 
 app.use(reqLogger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // simple ws
-wsServer.on('connection', function connection(ws) {
-	ws.send('{ "connection" : "ok"}');
-	// ws.on('open', function incoming(message) {
- //    	logger.info(message.data);
- //    });
-	ws.on('message', (message) => {
-		logger.info(message.data);
-	    ws.send(`Hello, you sent -> ${message}`);
-	});
+primus.on('connection', function connection(spark) {
+ logger.info('new connection');
+
+  spark.on('data', function data(packet) {
+  	logger.info('incoming:', packet);
+    spark.write('*');
+  });
+  spark.write('Hello world');
 });
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -67,18 +85,21 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
-module.exports = { app: app, server: server};
+module.exports = {
+	app: app,
+	server: server
+};
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,22 +1,41 @@
 /* global location, WebSocket */
 var host = location.origin.replace(/^https/, 'wss')
-var ws = new WebSocket(host);
+var primus = Primus.connect(location.origin);
 
-ws.onerror = (error) => console.log(error);
-ws.onopen = () => console.log('secure WebSocket connection established');
-ws.onclose = () => console.log('WebSocket connection closed');
 
-var textbox = $('input')
-var button = $('button')
+      primus.on('reconnect', function reconnect() {
+        console.log('reconnect', 'Reconnect', 'Starting the reconnect attempt, hopefully we get a connection!');
+      });
+      primus.on('online', function online() {
+        console.log('network', 'Online', 'We have regained control over our internet connection.');
+      });
+      primus.on('offline', function offline() {
+        console.log('network', 'Offline', 'We lost our internet connection.');
+      });
+      primus.on('open', function open() {
+        console.log('open', 'Open', 'The connection has been established.');
+    
+      });
+      primus.on('error', function error(err) {
+        console.log('error', 'Erorr', 'An unknown error has occured <code>'+ err.message +'</code>');
+      });
+      primus.on('data', function incoming(data) {
+      	console.log(performance.now());
+        console.log('data', 'Received data', 'string' === typeof data ? data : '<pre><code>'+ JSON.stringify(data, null, 2) +'</code></pre>');
+      });
+      primus.on('end', function end() {
+        console.log('end', 'End', 'The connection has ended.');
+       
+      });
+      primus.on('close', function end() {
+        console.log('close', 'close', 'We\'ve lost the connection to the server.');
+      });
 
-ws.onmessage = function(msg) {
-  var response = msg.data;
-  console.log(response);
-}
 
-var form = $('form');
-form.onsubmit = function() {
-  ws.send("gsdg");
-  return false;
-}
+
+$('#send').on( "click", function(event) {
+  console.log('submit');
+  console.log(performance.now());
+  primus.write('message');
+});
 
