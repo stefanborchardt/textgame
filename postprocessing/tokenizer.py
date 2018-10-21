3384486e6cca73e432f96a33278493194f312f95
Stefan Borchardt <stefan.borchardt@gmail.com>
2018-10-21 19:54:52 +0200

    postprocess with respect to image category

--- postprocessing/tokenizer.py
+++ postprocessing/tokenizer.py
@@ -1,23 +1,35 @@
 import json
 import nltk
 
-# import matplotlib.pyplot as plt
-
 file = open("logs/postprocess.log", "r", encoding="utf-8")
-tknzr = nltk.TweetTokenizer() # tokenize.WordPunctTokenizer()
-tokens = []
-for line in file:
-    jline = json.loads(line)
+tknzr = nltk.TweetTokenizer()  # tokenize.WordPunctTokenizer()
+
+tokens = {}
+id_map = {}
+cur_tokens = []
+for l in file:
+    line = json.loads(l)
+    if "for" in line:
+        id_map = line
     if "txt" in line:
-        tokens += tknzr.tokenize(jline["txt"])
-    #     print(tokens)
-    # if "currentEvent" in line:
-    #     curEvt = jline["currentEvent"]
-    #     print(list(curEvt.keys())[0] +
-    #           ": " + str(list(curEvt.values())[0]))
-stop_words = nltk.corpus.stopwords.words('english')
-# print(stop_words)
-filtered_tokens = [w.lower() for w in tokens if (w.lower() not in stop_words)]
-# print(filtered_tokens)
-fd = nltk.FreqDist(filtered_tokens)
-fd.plot(50,cumulative=False)
+        cur_tokens += tknzr.tokenize(line["txt"])
+    if "currentEvent" in line:
+        cur_evt = line["currentEvent"]
+        evt_key = list(cur_evt.keys())[0]
+        evt_val = list(cur_evt.values())[0]
+        if evt_key != "undo" and evt_key != "joker":
+            category = id_map[evt_key].split("/")[0]
+            tokens[category] = cur_tokens + tokens.get(category, [])
+            print("=======================")
+            print(category)
+            print(cur_tokens)
+            cur_tokens = []
+
+stop_words = nltk.corpus.stopwords.words("english")
+filtered_tokens = {}
+for cat in tokens.keys():
+    filtered_tokens[cat] = [w.lower()
+                            for w in tokens[cat] if (w.lower() not in stop_words)]
+
+fd = nltk.FreqDist(filtered_tokens["berry"])
+fd.plot(50, cumulative=False)
