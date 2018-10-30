6297e4c758016c9d843489736da651a371ba0b95
Stefan Borchardt <stefan.borchardt@gmail.com>
2018-10-30 14:28:20 +0100

    add corpus statistics for game levels

--- postprocessing/tokenizer.py
+++ postprocessing/tokenizer.py
@@ -4,12 +4,14 @@ import os
 import fileinput
 
 
-tknzr = nltk.TweetTokenizer()  # tokenize.WordPunctTokenizer()
+tknzr = nltk.TweetTokenizer()  
+# tknzr = nltk.WordPunctTokenizer()
 tokens = {}
 all_tokens = []
 id_map = {}
 cur_tokens = []  # tokens since last selection
 msg_lengths = []
+cur_game = ''
 
 os.chdir('postprocessing/keep')
 files = os.listdir()
@@ -19,6 +21,7 @@ with fileinput.input(files) as f:
         if "for" in line:
             # image id to file name mapping
             id_map = line
+            cur_game = line["for"].split(":")[0]
         if "txt" in line:
             # a message
             tks = tknzr.tokenize(line["txt"])
@@ -30,9 +33,11 @@ with fileinput.input(files) as f:
             evt_key = list(cur_evt.keys())[0]
             evt_val = list(cur_evt.values())[0]
             if evt_key != "undo" and evt_key != "joker":
-                # image selection, tokens since last selection are put into category of image
+                # image selection, tokens since last selection
+                # are put into category of image and game level
                 category = id_map[evt_key].split("/")[0]
                 tokens[category] = tokens.get(category, []) + cur_tokens
+                tokens[cur_game] = tokens.get(cur_game, []) + cur_tokens
                 cur_tokens = []
 
 stop_words = nltk.corpus.stopwords.words("english")
@@ -43,18 +48,29 @@ for cat in tokens.keys():
 all_filtered_tokens = [w.lower()
                        for w in all_tokens if (w.lower() not in stop_words)]
 
-print('total tokens {0}'.format(len(all_tokens)))
-print('distinct tokens {0}'.format(len(set(all_tokens))))
-# msglenfd = nltk.FreqDist(msg_lengths)
-# msglenfd.plot(50, cumulative=False)
 
-fd = nltk.FreqDist(all_filtered_tokens)
-fd.plot(50, cumulative=False)
+print("total tokens {0}".format(len(all_tokens)))
+print("distinct tokens {0}".format(len(set(all_tokens))))
+
+msglenfd = nltk.FreqDist(msg_lengths)
+msglenfd.plot(50, cumulative=False)
+
+# fd = nltk.FreqDist(all_filtered_tokens)
+# fd.plot(50, cumulative=False)
+
 # fd1 = nltk.FreqDist(filtered_tokens["berry"])
 # fd1.plot(50, cumulative=False)
-# fd2 = nltk.FreqDist(filtered_tokens["dog"])
-# fd2.plot(50, cumulative=False)
+fd2 = nltk.FreqDist(filtered_tokens["dog"])
+fd2.plot(50, cumulative=False)
 # fd3 = nltk.FreqDist(filtered_tokens["flower"])
 # fd3.plot(50, cumulative=False)
 # fd4 = nltk.FreqDist(filtered_tokens["bird"])
 # fd4.plot(50, cumulative=False)
+
+# fdg1 = nltk.FreqDist(filtered_tokens["first"])
+# fdg1.plot(50, cumulative=False)
+fdg2 = nltk.FreqDist(filtered_tokens["second"])
+fdg2.plot(50, cumulative=False)
+
+# bg = nltk.collocations.BigramCollocationFinder.from_words(filtered_tokens["berry"])
+# bg.ngram_fd.plot(50, cumulative=False)
